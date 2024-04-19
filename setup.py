7e92842fa826586e94fdd6e890c4612c6275fe60
maripnunes <165323586+maripnunes@users.noreply.github.com>
2024-04-19 14:34:52 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
 	    name="tabsus", # Replace with your username
-    version="1.0.0",
+    version="1.0.1",
     author="Mariana Nunes",
     author_email="marianapnunes@gmail.com",
     description="Library for processing DATASUS database files",
