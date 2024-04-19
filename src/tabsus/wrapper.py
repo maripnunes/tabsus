ab5c2c03acbe6e8d10f6ef7bd6e1b5692fa57fb7
maripnunes <165323586+maripnunes@users.noreply.github.com>
2024-04-19 14:54:42 -0300

    Update wrapper.py

--- src/tabsus/wrapper.py
+++ src/tabsus/wrapper.py
@@ -54,33 +54,28 @@ class TabSus:
 
 
 class Database:
-    #def __init__(self, name, tab_url):
-    #    self.name = name
-    #    self.tab_url = urllib.parse.urlparse(tab_url)
-    #
-    #    self.file_path = None
-    #    if self.tab_url.scheme == 'file':
-    #        self.file_path = os.path.abspath(os.path.join(self.tab_url.netloc, self.tab_url.path))
-    
-    def __init__(self, name):
-        self.name = name        
-        self.file_path = os.path.abspath(os.path.join(self.name))
-        
+    def __init__(self, name, tab_url):
+        self.name = name
+        self.tab_url = urllib.parse.urlparse(tab_url)
+
+        self.file_path = None
+        if self.tab_url.scheme == 'file':
+            self.file_path = os.path.abspath(os.path.join(self.tab_url.netloc, self.tab_url.path))
+
     @property
     def tabsus(self):
         return self.get_tabsus()
 
     def get_tabsus(self):
-        #filename = os.path.join(tabsus.DOWNLOAD_PATH, f"{self.name}.zip")
-        filename = os.path.join(tabsus.DOWNLOAD_PATH, f"{self.name}")
-        
+        filename = os.path.join(tabsus.DOWNLOAD_PATH, f"{self.name}.zip")
+
         if not self.file_path:
             if os.path.exists(filename):
                 self.file_path = os.path.abspath(filename)
-            #else:
-            #    os.makedirs(os.path.dirname(filename), exist_ok=True)
-            #    urllib.request.urlretrieve(self.tab_url.geturl(), filename)
-            #    self.file_path = os.path.abspath(filename)
+            else:
+                os.makedirs(os.path.dirname(filename), exist_ok=True)
+                urllib.request.urlretrieve(self.tab_url.geturl(), filename)
+                self.file_path = os.path.abspath(filename)
 
         try:
             return TabSus(self.file_path)
@@ -89,14 +84,15 @@ class Database:
             os.remove(self.file_path)
             return self.get_tabsus()
 
+
 DATABASES = [
-    Database('SIH', '/home/ubuntu/DATASUS/SIHSUS/TAB_SIH.zip'),
-    Database('SIA', '/home/ubuntu/DATASUS/SIASUS/TAB_SIA.zip'),
-    #Database('SIM', 'ftp://ftp.datasus.gov.br/dissemin/publicos/SIM/CID10/TAB/OBITOS_CID10_TAB.ZIP'),
-    #Database('SINASC', 'ftp://ftp.datasus.gov.br/dissemin/publicos/SINASC/1996_/Auxiliar/NASC_NOV_TAB.zip'),
-    #Database('SINANNET', 'ftp://ftp.datasus.gov.br/dissemin/publicos/SINAN/AUXILIAR/TAB_SINANNET.zip'),
-    #Database('SINANONLINE', 'ftp://ftp.datasus.gov.br/dissemin/publicos/SINAN/AUXILIAR/TAB_SINANONLINE.zip'),
-    #Database('ANS', 'http://ftp.dadosabertos.ans.gov.br/FTP/Base_de_dados/Microdados/arquivos_auxiliares_de_tab_def_e_cnv/arquivos_auxiliares_de_tabulacao.zip')
+    Database('SIH', 'ftp://ftp.datasus.gov.br/dissemin/publicos/SIHSUS/200801_/Auxiliar/TAB_SIH.zip'),
+    Database('SIA', 'ftp://ftp.datasus.gov.br/dissemin/publicos/SIASUS/200801_/Auxiliar/TAB_SIA.zip'),
+    Database('SIM', 'ftp://ftp.datasus.gov.br/dissemin/publicos/SIM/CID10/TAB/OBITOS_CID10_TAB.ZIP'),
+    Database('SINASC', 'ftp://ftp.datasus.gov.br/dissemin/publicos/SINASC/1996_/Auxiliar/NASC_NOV_TAB.zip'),
+    Database('SINANNET', 'ftp://ftp.datasus.gov.br/dissemin/publicos/SINAN/AUXILIAR/TAB_SINANNET.zip'),
+    Database('SINANONLINE', 'ftp://ftp.datasus.gov.br/dissemin/publicos/SINAN/AUXILIAR/TAB_SINANONLINE.zip'),
+    Database('ANS', 'http://ftp.dadosabertos.ans.gov.br/FTP/Base_de_dados/Microdados/arquivos_auxiliares_de_tab_def_e_cnv/arquivos_auxiliares_de_tabulacao.zip')
 ]
 
 DATABASES = {it.name: it for it in DATABASES}
