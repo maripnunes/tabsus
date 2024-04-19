fa2cf91016d4e6688a831f1febd6e889e2cc39a8
maripnunes <165323586+maripnunes@users.noreply.github.com>
2024-04-19 12:51:25 -0300

    Update wrapper.py

--- src/tabsus/wrapper.py
+++ src/tabsus/wrapper.py
@@ -85,8 +85,8 @@ class Database:
             return self.get_tabsus()
 
 DATABASES = [
-    Database('SIH', '/home/ubuntu/DATASUS/SIHSUS/TAB_SIH.zip'),
-    Database('SIA', '/home/ubuntu/DATASUS/SIASUS/TAB_SIA.zip'),
+    Database('SIH', '//home/ubuntu/DATASUS/SIHSUS/TAB_SIH.zip'),
+    Database('SIA', '//home/ubuntu/DATASUS/SIASUS/TAB_SIA.zip'),
     #Database('SIM', 'ftp://ftp.datasus.gov.br/dissemin/publicos/SIM/CID10/TAB/OBITOS_CID10_TAB.ZIP'),
     #Database('SINASC', 'ftp://ftp.datasus.gov.br/dissemin/publicos/SINASC/1996_/Auxiliar/NASC_NOV_TAB.zip'),
     #Database('SINANNET', 'ftp://ftp.datasus.gov.br/dissemin/publicos/SINAN/AUXILIAR/TAB_SINANNET.zip'),
